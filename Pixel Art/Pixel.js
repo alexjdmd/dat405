303d491abba200ec31a4197b81eee8bce30ef673
Alex Jones <alexjdigitalmedia@gmail.com>
2018-10-24 12:03:10 +0100

    Created Functions to render the different coloured pixels

--- Pixel Art/Pixel.js
+++ Pixel Art/Pixel.js
@@ -1,4 +1,3 @@
-//https://github.com/stavrosdidakis/DAT-GAD-405_2017
 //https://p5js.org
 
 //DAT405 / GAD405
@@ -11,16 +10,123 @@ function setup() {
   //Print a message to the console - to view using Chrome:
   //View > Developer > Developer Tools > Console
   console.log("Initialization: OK")
-  createCanvas(500,500);
+  createCanvas(200,200);
+    background(213, 218, 240);
+  drawgreen();
+drawblue();
 }
 
-//Rendering function
-function draw() {
-  //Set a background color
-  background(255, 0, 205);
+//Rendering functions
 
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Rendering...")
-  noLoop();
+function drawgreen() {
+  //A function that draws all of the green "pixels" in the image
+fill(36,177,78);
+noStroke();
+rect(0 ,60 ,10 ,10);
+rect(0 ,70 ,10 ,10);
+rect(0 ,80 ,10 ,10);
+rect(0 ,90 ,10 ,10);
+rect(0 ,100 ,10 ,10);
+rect(0 ,110 ,10 ,10);
+rect(0 ,120 ,10 ,10);
+
+rect(10 ,60 ,10 ,10);
+rect(10 ,70 ,10 ,10);
+rect(10 ,80 ,10 ,10);
+rect(10 ,90 ,10 ,10);
+rect(10 ,100 ,10 ,10);
+rect(10 ,110 ,10 ,10);
+rect(10 ,120 ,10 ,10);
+
+rect(20 ,60 ,10 ,10);
+rect(20 ,70 ,10 ,10);
+rect(20 ,80 ,10 ,10);
+rect(20 ,90 ,10 ,10);
+rect(20 ,100 ,10 ,10);
+rect(20 ,110 ,10 ,10);
+rect(20 ,120 ,10 ,10);
+
+rect(30 ,30 ,10 ,10);
+rect(30 ,40 ,10 ,10);
+rect(30 ,50 ,10 ,10);
+rect(30 ,60 ,10 ,10);
+rect(30 ,70 ,10 ,10);
+rect(30 ,80 ,10 ,10);
+rect(30 ,90 ,10 ,10);
+rect(30 ,100 ,10 ,10);
+rect(30 ,110 ,10 ,10);
+rect(30 ,120 ,10 ,10);
+rect(30 ,130 ,10 ,10);
+rect(30 ,140 ,10 ,10);
+rect(30 ,150 ,10 ,10);
+rect(30 ,160 ,10 ,10);
+rect(30 ,170 ,10 ,10);
+
+rect(40 ,30 ,10 ,10);
+rect(40 ,40 ,10 ,10);
+rect(40 ,50 ,10 ,10);
+rect(40 ,60 ,10 ,10);
+rect(40 ,70 ,10 ,10);
+rect(40 ,80 ,10 ,10);
+rect(40 ,90 ,10 ,10);
+rect(40 ,100 ,10 ,10);
+rect(40 ,110 ,10 ,10);
+rect(40 ,120 ,10 ,10);
+rect(40 ,130 ,10 ,10);
+rect(40 ,140 ,10 ,10);
+rect(40 ,150 ,10 ,10);
+rect(40 ,160 ,10 ,10);
+rect(40 ,170 ,10 ,10);
+}
+function drawblue(){
+  fill(63,71,204);
+  noStroke();
+  rect(0 ,130 ,10 ,10);
+  rect(0 ,140 ,10 ,10);
+  rect(0 ,150 ,10 ,10);
+  rect(0 ,160 ,10 ,10);
+  rect(0 ,170 ,10 ,10);
+  rect(0 ,180 ,10 ,10);
+  rect(0 ,190 ,10 ,10);
+
+  rect(10 ,130 ,10 ,10);
+  rect(10 ,140 ,10 ,10);
+  rect(10 ,150 ,10 ,10);
+  rect(10 ,160 ,10 ,10);
+  rect(10 ,170 ,10 ,10);
+  rect(10 ,180 ,10 ,10);
+  rect(10 ,190 ,10 ,10);
+
+  rect(20 ,130 ,10 ,10);
+  rect(20 ,140 ,10 ,10);
+  rect(20 ,150 ,10 ,10);
+  rect(20 ,160 ,10 ,10);
+  rect(20 ,170 ,10 ,10);
+//Bottom two pixels
+  rect(20 ,180 ,10 ,10);
+  rect(20 ,190 ,10 ,10);
+  rect(30 ,180 ,10 ,10);
+  rect(30 ,190 ,10 ,10);
+  rect(40 ,180 ,10 ,10);
+  rect(40 ,190 ,10 ,10);
+  rect(50 ,180 ,10 ,10);
+  rect(50 ,190 ,10 ,10);
+  rect(60 ,180 ,10 ,10);
+  rect(60 ,190 ,10 ,10);
+  rect(70 ,180 ,10 ,10);
+  rect(70 ,190 ,10 ,10);
+  rect(80 ,180 ,10 ,10);
+  rect(80 ,190 ,10 ,10);
+  rect(90 ,180 ,10 ,10);
+  rect(90 ,190 ,10 ,10);
+  rect(100 ,180 ,10 ,10);
+  rect(100 ,190 ,10 ,10);
+  rect(110 ,180 ,10 ,10);
+  rect(110 ,190 ,10 ,10);
+  rect(120 ,180 ,10 ,10);
+  rect(120 ,190 ,10 ,10);
+  rect(130 ,180 ,10 ,10);
+  rect(130 ,190 ,10 ,10);
+  rect(140 ,180 ,10 ,10);
+  rect(140 ,190 ,10 ,10);
 }
