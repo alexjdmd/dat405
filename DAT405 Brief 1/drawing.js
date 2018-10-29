4bc1242fc9eb13266816a73b6c71f69119b7a1cd
Alex Jones <alexjdigitalmedia@gmail.com>
2018-10-29 12:14:41 +0000

    a

---
+++ DAT405 Brief 1/drawing.js
@@ -0,0 +1,16 @@
+//Initialization function
+function setup() {
+
+  createCanvas(594,841);
+}
+
+//Rendering function
+function draw() {
+  //Set a background color
+  background(25, 0, 205);
+
+  //Print a message to the console - to view using Chrome:
+  //View > Developer > Developer Tools > Console
+  console.log("Rendering...")
+  noLoop();
+}
