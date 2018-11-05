e1fd9a8550290e263569ea32cfb7f763b69b5276
Alex Jones <alexjdigitalmedia@gmail.com>
2018-11-05 10:19:32 +0000

    Scaled the window so that the work changes according to the size of the browser

--- DAT405 Brief 1/drawing.js
+++ DAT405 Brief 1/drawing.js
@@ -1,28 +1,30 @@
-//Initialization function
-function setup() {
-framerate(30);
-  createCanvas(594,841);
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  background(random(255),random(255),random(255));
+  frameRate(0.5);
+
   draw();
-//draw1();
-}
 
-//Rendering function
-function draw() {
-  //Set a background color
-  background(25, 0, 205);
-
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Rendering...")
-  noLoop();
 }
-function draw1(){
- // draw() loops forever, until stopped
- var yPos = 0;
-  yPos = 1;
-  if (yPos < 0) {
-    yPos = 300;
+function draw(){
+  var x = windowWidth/5
+  //for (var i = 0; i<500/2;i++){
+    for(var g = 0; g < windowWidth; g++){
+      //calling the two differnt functions, one for the border, the other for the funky squares
+      drawrectfunky();
+    }
+
+//  }
+  function drawrectfunky(){
+    fill(random(255),random(255),random(255));
+    noStroke();
+    rect(g*x, 0 , x ,windowHeight)
+    noLoop();
   }
-  line(10, yPos, 500, 400);
-  noLoop();
+
 }
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
+}
+//have the mouse move and change the colour of the square that it is hovering over, potentially adding a stroke to it as well.
