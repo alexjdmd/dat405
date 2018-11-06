d84091f16d66def4156b7f84c8fae43bc8b38a3f
Alex Jones <alexjdigitalmedia@gmail.com>
2018-11-06 17:12:50 +0000

    the mouse position is now recorded and a rectangle is created at the current position of the cursor.

--- DAT405 Brief 1/drawing.js
+++ DAT405 Brief 1/drawing.js
@@ -2,29 +2,43 @@
 function setup(){
   createCanvas(windowWidth,windowHeight);
   background(random(255),random(255),random(255));
-  frameRate(0.5);
-
-  draw();
+  frameRate(100);
 
 }
 function draw(){
-  var x = windowWidth/5
-  //for (var i = 0; i<500/2;i++){
-    for(var g = 0; g < windowWidth; g++){
-      //calling the two differnt functions, one for the border, the other for the funky squares
-      drawrectfunky();
-    }
-
-//  }
-  function drawrectfunky(){
-    fill(random(255),random(255),random(255));
-    noStroke();
-    rect(g*x, 0 , x ,windowHeight)
-    noLoop();
-  }
-
+  //setting the size of the squares relative to the size of the window`
+var rectX = windowWidth/500
+var rectY = windowHeight/5
+var x1 = map(mouseX, 0 , windowWidth, 0,windowWidth)
+//storing the current x coordinate of the mouse in a variable
+var x2 = map(mouseY, 0 , windowHeight,0,windowHeight)
+//storing the current y coordinate of the mouse in a variable
+fill(0)
+noStroke();
+rectMode(RADIUS);
+//changing the mode of the rectangle so that it is in the centre of the cursor rather than the corner.
+rect(x1, x2,rectX,rectX);
 }
+function pos_to_neg(mouseX)
+{
+return -Math.abs(mouseX);
+}
+// A function that resizes the canvas depending on the size of the window.
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
 }
 //have the mouse move and change the colour of the square that it is hovering over, potentially adding a stroke to it as well.
+
+/////////////////////////////////////////////////////////////////////////////////////////////OLD CODE
+
+  //  for(var g = 0; g < windowWidth; g++){
+      //calling the function that draws the rectangles
+  //    drawrectfunky();
+//    }
+
+//  function drawrectfunky(){
+//    fill(random(255));
+//    noStroke();
+//    rect(g*rectX, 0 , rectX ,windowHeight)
+//    noLoop();
+//  }
