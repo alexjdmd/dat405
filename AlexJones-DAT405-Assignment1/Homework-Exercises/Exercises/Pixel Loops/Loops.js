71f374badadc9decb9fe349016bdb3b31c75bc21
Alex Jones <alexjdigitalmedia@gmail.com>
2018-11-21 19:12:53 +0000

    2 grids of squares, one with a fixed colour and the other with a random fill.

---
+++ AlexJones-DAT405-Assignment1/Homework-Exercises/Exercises/Pixel Loops/Loops.js
@@ -0,0 +1,24 @@
+function setup(){
+  createCanvas(1920,1080);
+  background(random(255),random(255),random(255));
+  frameRate(30);
+}
+function draw(){
+  var x = 100
+  var height = 100
+  for (var i = 0; i<12;i++){
+    for(var g = 0; g < 10; g++){
+      //calling the two differnt functions, one for the border, the other for the funky squares
+      drawrectsolid();
+      drawrectfunky();
+    }
+  }
+  function drawrectsolid(){
+    fill(225,226,140);
+    rect(g*x, i*height, height,height)
+  }
+  function drawrectfunky(){
+    fill(random(255),random(255),random(255));
+    rect(g*x + 25, i*height + 25, height - 50,height - 50)
+  }
+}
